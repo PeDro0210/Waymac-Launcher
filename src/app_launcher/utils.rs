62ff8944757868e7a82f02e4b440b0619ad1040b
PeDro0210 <pedroruben021@gmail.com>
2026-04-21 00:11:55 -0600

    chore: defining simple desktop entry targets per OS

--- src/app_launcher/utils.rs
+++ src/app_launcher/utils.rs
@@ -1,4 +1,14 @@
+use std::env::consts::OS;
+
 pub enum DesktopEntriesTarget {
     MacOS,
     XDG,
+}
+
+pub fn desktop_entry_target() -> DesktopEntriesTarget {
+    match OS {
+        "linux" => DesktopEntriesTarget::XDG, // if u ain't using XDG, then this ain't your place
+        "macos" => DesktopEntriesTarget::MacOS,
+        _ => panic!("Desktop Entry Target not supported"),
+    }
 }
--- src/display_servers/mod.rs
+++ src/display_servers/mod.rs
@@ -1,4 +1,2 @@
 pub mod wayland;
 pub mod xorg;
-
-pub struct DesktopEntry {}
